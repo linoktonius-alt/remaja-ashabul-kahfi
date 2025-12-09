0f791593b3db392477fa66539c47c9da587c8e3d
cevin rizki octavian <linoktonius@gmail.com>
2025-12-09 14:52:45 +0700

    menambahkan page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,5 +1,31 @@
 import streamlit as st
+# Mengatur tampilan halaman
+st.set_page_config(
+    page_title="Selamat Datang",
+    layout="wide",
+)
 
-st.title("Welcome to the Lin's Home Page")
-st.write("This is main landing page of the app.")
+st.title("Selamat datang di remaja-ashabul-kahfi")
 
+st.markdown("---")
+
+st.markdown(
+    """
+    <div style='text-align: center; margin-top: 50px;'>
+        <p style='font-size: 24px;'>dipersembahkan oleh:</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div style='text-align: center; font-size: 20px;'>
+        <p>cevin</p>
+        <p>yana</p>
+        <p>hapis</p>
+        <p>ucup</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
