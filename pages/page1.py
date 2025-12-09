f5bbd5a074859b65ebe61c5aef84260d91c261f8
cevin rizki octavian <linoktonius@gmail.com>
2025-12-09 15:10:53 +0700

    menambahkan page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -9,6 +9,27 @@ st.title("Selamat datang di remaja-ashabul-kahfi")
 
 st.markdown("---")
 
+st.write("### Perkenalan")
+
+st.write(
+    "Selamat datang di Website Monitoring Perkembangan Ekonomi Indonesia.
+Situs ini dibuat untuk memberikan informasi yang ringkas, akurat, dan mudah dipahami mengenai kondisi ekonomi Indonesia dari waktu ke waktu.
+
+Dalam website ini, Anda dapat menemukan berbagai indikator ekonomi penting yang menjadi dasar dalam analisis makroekonomi nasional, antara lain:
+
+Inflasi – menggambarkan tingkat kenaikan harga barang dan jasa.
+
+Suku Bunga – mencerminkan kebijakan moneter dan memengaruhi aktivitas kredit serta konsumsi.
+
+Kurs Rupiah – menunjukkan nilai tukar rupiah terhadap mata uang asing yang berdampak pada perdagangan dan stabilitas harga.
+
+Produk Domestik Regional Bruto (PDRB) – mengilustrasikan kinerja ekonomi di tingkat daerah.
+
+Pertumbuhan Ekonomi – memberikan gambaran mengenai ekspansi atau perlambatan aktivitas ekonomi nasional.
+
+Website ini dirancang untuk membantu mahasiswa, peneliti, pembuat kebijakan, maupun masyarakat umum dalam memahami kondisi ekonomi terkini berbasis data yang tersusun secara visual dan informatif."
+)
+
 st.markdown(
     """
     <div style='text-align: center; margin-top: 50px;'>
