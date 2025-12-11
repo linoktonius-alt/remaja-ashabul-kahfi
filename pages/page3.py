8489f3361bf34dbd25690ba2ed5d71a17c53f479
Hafizh Alfani Rizqi <765hafizh@gmail.com>
2025-12-11 13:38:12 +0700

    menambakan file page 3

--- pages/page3.py
+++ pages/page3.py
@@ -15,7 +15,21 @@ oleh mahasiswa, peneliti, maupun masyarakat umum.
 
 st.markdown("---")
 
-st.header("Anggota Tim")
+st.header("🎯 Visi dan Misi")
+st.success("""
+**Visi:**  
+Menyediakan visualisasi data ekonomi yang menarik, informatif, dan mudah dipahami oleh semua kalangan.
+
+**Misi:**  
+- 🔍 Mengolah data ekonomi Indonesia dengan akurat  
+- 📊 Menyajikan grafik yang interaktif dan mudah diinterpretasi  
+- 💡 Membantu mahasiswa dan masyarakat memahami kondisi ekonomi  
+- 🌐 Membangun platform data yang modern dan mudah diakses  
+""")
+
+st.markdown("---")
+
+st.header("😎Anggota Tim")
 st.write("Berikut adalah anggota yang terlibat dalam pembuatan website ini:")
 
 col1, col2, col3, col4 = st.columns(4)
@@ -23,45 +37,46 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.subheader("Cevin Rizki Octavian")
     st.write("""
-    **NIM:021002305005**  
-    **Program Studi:** Ekonomi Pembangunan   
+    🆔**NIM:021002305005**  
+    🎓**Program Studi:** Ekonomi Pembangunan   
     """)
 
 with col2:
     st.subheader("Haizar Yusuf Fahrezzy")
     st.write("""
-     🆔 **NIM:021002305020**  
+    🆔 **NIM:021002305020**  
     🎓**Program Studi:** Ekonomi Pembangunan    
     """)
 
 with col3:
     st.subheader("Hafizh Alfani Rizqi")
     st.write("""
-    **NIM:021002305016**  
-    **Program Studi:** Ekonomi Pembangunan   
+    🆔**NIM:021002305016**  
+    🎓**Program Studi:** Ekonomi Pembangunan   
     """)
 
 with col4:
     st.subheader("Rizkyana Maharani")
     st.write("""
-    **NIM:021002305010**  
-    **Program Studi:** Ekonomi Pembangunan   
+    🆔**NIM:021002305010**  
+    🎓**Program Studi:** Ekonomi Pembangunan   
     """)
 
 st.markdown("---")
 
-st.header("Tools yang Digunakan")
+st.header("🧰Tools yang Digunakan")
 st.write("""
-- Python  
-- Pandas  
-- Matplotlib  
-- Plotly  
-- Streamlit  
-- Jupyter Lab  
+- 🐍Python  
+- 🟦Pandas  
+- 📈Matplotlib  
+- 📉Plotly  
+- 🟩Streamlit  
+- 📒Jupyter Lab  
 """)
 
 st.markdown("---")
 
-# ====== QUOTES ======
-st.info("“Sebutlah Namanya Tetap di Jalannya, Kelak Kau Meningat” – Pemuda Ashabul Kahfi ♥")
+st.info("“Sebutlah Namanya Tetap di Jalannya, Kelak Kau Kan Meningat” – Pemuda Ashabul Kahfi ♥")
+
+st.markdown("Terima kasih telah mengunjungi halaman profil kami! 🙏❤️")
 
